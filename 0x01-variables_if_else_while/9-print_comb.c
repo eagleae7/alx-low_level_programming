9e8791ac49bb4a9a03892fa50ed000cec3b48e8b
eagleae7 <spotarinz@gmail.com>
2022-10-17 09:55:27 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 
 /**
  * main -  program that prints all possible combinations of single-digit
- * Numbers must be separated by ,followed by a space
+ * Numbers must be separated by , followed by a space
  * Numbers should be printed in ascending order
  * You can only use the putchar function
  * Return: 0
