4ef33fcdf220f7c82076bb019d4b1db8c409119b
eagleae7 <spotarinz@gmail.com>
2022-10-17 10:28:53 +0100

    new update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,11 +3,11 @@
 /**
  * main - program that prints all possible different combinations of
  * three digits
- * 012, 120, 102, 021, 201, 210 are considered the 
+ * 012, 120, 102, 021, 201, 210 are considered the
  * same combination of the three digits 0, 1 and 2
  * Print only the smallest combination of three digits
  * Numbers should be printed in ascending order, with three digits
- * You can only use the putchar function 
+ * You can only use the putchar function
  * (every other function (printf, puts, etc…) is forbidden)
  * You can only use putchar six times maximum in your code
  * You are not allowed to use any variable of type char
