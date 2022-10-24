eb575e2e0c19fd4023f50ddac779739ae1e9cb72
eagleae7 <spotarinz@gmail.com>
2022-10-24 22:01:25 +0100

    pointers arrays strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,19 +3,19 @@
 /**
  * _atoi - function that convert a string to an integer
  * @s: string to convert
- * Return: intg
+ * Return: int
  */
 
 int _atoi(char *s)
 {
     int i = 0;
     int n = 0;
-    int d = 1;
+    int signo = 1;
 
     while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
     {
         if (s[i] == '-')
-            d *= -1;
+            signo *= -1;
         i++;
     }
     while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
@@ -31,6 +31,6 @@ int _atoi(char *s)
             i++;
         }
     }
-    d *= -1;
-    return (n * d);
+    signo *= -1;
+    return (n * signo);
 }
