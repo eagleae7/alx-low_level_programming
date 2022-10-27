25870a70a365be32f6928c3ba8f8268ad367e638
eagleae7 <spotarinz@gmail.com>
2022-10-27 22:17:28 +0100

    pointers arrays strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,27 +5,40 @@
  * @s: pointer to char
  * Return: char
  */
-
-char *rot13(char *s)
+char *rot13(char *str)
 {
-    int i = 0;
-    int j = 0;
-    char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-    char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+    int indx1 = 0, indx2;
+    char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+                         'G', 'H', 'I', 'J', 'K', 'L',
+                         'M', 'N', 'O', 'P', 'Q', 'R',
+                         'S', 'T', 'U', 'V', 'W', 'X',
+                         'Y', 'Z', 'a', 'b', 'c', 'd',
+                         'e', 'f', 'g', 'h', 'i', 'j',
+                         'k', 'l', 'm', 'n', 'o', 'p',
+                         'q', 'r', 's', 't', 'u', 'v',
+                         'w', 'x', 'y', 'z'};
+    char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+                         'T', 'U', 'V', 'W', 'X', 'Y',
+                         'Z', 'A', 'B', 'C', 'D', 'E',
+                         'F', 'G', 'H', 'I', 'J', 'K',
+                         'L', 'M', 'n', 'o', 'p', 'q',
+                         'r', 's', 't', 'u', 'v', 'w',
+                         'x', 'y', 'z', 'a', 'b', 'c',
+                         'd', 'e', 'f', 'g', 'h', 'i',
+                         'j', 'k', 'l', 'm'};
 
-    while (*(s + i) != '\0')
+    while (str[indx1])
     {
-        while (*(l + j) != '\0')
+        for (indx2 = 0; indx2 < 52; indx2++)
         {
-            if (*(s + i) == *(l + j))
+            if (str[indx1] == alphabet[indx2])
             {
-                *(s + i) = *(r13 + j);
+                str[indx1] = rot13key[indx2];
                 break;
             }
-            j++;
         }
-        j = 0;
-        i++;
+
+        indx1++;
     }
-    return (s);
+    return (str);
 }
