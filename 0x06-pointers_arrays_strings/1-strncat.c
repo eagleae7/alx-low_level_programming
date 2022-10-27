83d4ba93629a9f43e6ed8b4b7258e6f51150cb7d
eagleae7 <spotarinz@gmail.com>
2022-10-27 20:26:42 +0100

    pointers arrays strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,30 +2,27 @@
 
 /**
  * _strncat - concatenates two string together
- * @src: parameter to append to dest
- * @dest: parameter to be appended to
- * @n: maximum number bytes usable
+ * @dest: pointer to destination char
+ * @src: pointer to source char
+ * @n: number of bytes to receive
  * Return: Always 0.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0')
-	{
+	while (*(dest + i) != '\0')
 		i++;
-	}
-
-	while (j < n && src[j] != '\0')
+	while (*(src + j) != '\0')
 	{
-		dest[i] += src[j];
-		i++;
+		if (j < n)
+		{
+			*(dest + i) = *(src + j);
+			i++;
+		}
 		j++;
 	}
-
-	dest += '\0';
-
 	return (dest);
 }
