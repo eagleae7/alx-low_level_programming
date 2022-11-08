d12c6e1e880ef95c2edec24620ea5558e1bb9758
eagleae7 <spotarinz@gmail.com>
2022-11-08 22:09:38 +0100

    malloc_free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,34 +8,34 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, size, k;
-    char *newStr;
+	int i, j, size, k;
+	char *newStr;
 
-    k = 0;
-    size = 0;
+	k = 0;
+	size = 0;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-        for (j = 0; av[i][j]; j++)
-            size++;
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j]; j++)
+			size++;
 
-    newStr = malloc((size + ac + 1) * sizeof(char));
-    if (newStr == NULL)
-        return (NULL);
+	newStr = malloc((size + ac + 1) * sizeof(char));
+	if (newStr == NULL)
+		return (NULL);
 
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            newStr[k] = av[i][j];
-            k++;
-        }
-        newStr[k] = '\n';
-        k++;
-    }
-    newStr[k] = '\0';
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			newStr[k] = av[i][j];
+			k++;
+		}
+		newStr[k] = '\n';
+		k++;
+	}
+	newStr[k] = '\0';
 
-    return (newStr);
+	return (newStr);
 }
