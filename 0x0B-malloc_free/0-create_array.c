e672362a6b7b8629b0af91a47f8b0a18e845dc4c
eagleae7 <spotarinz@gmail.com>
2022-11-08 21:33:12 +0100

    malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,15 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
+	char *arr;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
-    arr = malloc(size * sizeof(char));
-    if (arr == NULL)
-        return (NULL);
-    for (i = 0; i < size; i++)
-        arr[i] = c;
-    return (arr);
+	if (size == 0)
+		return (NULL);
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		*(arr + i) = c;
+	return (arr);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,19 +8,19 @@
  */
 char *_strdup(char *str)
 {
-    int strSize, i;
-    char *copyStr;
+	int strSize, i;
+	char *copyStr;
 
-    strSize = 0;
-    if (str == NULL)
-        return (NULL);
-    while (str[strSize] != '\0')
-        strSize++;
-    strSize++; /* add 1 to print NULL*/
-    copyStr = malloc((strSize) * sizeof(char));
-    if (copyStr == NULL)
-        return (0);
-    for (i = 0; i < (strSize); i++)
-        *(copyStr + i) = *(str + i);
-    return (copyStr);
+	strSize = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[strSize] != '\0')
+		strSize++;
+	strSize++; /* add 1 to print NULL*/
+	copyStr = malloc((strSize) * sizeof(char));
+	if (copyStr == NULL)
+		return (0);
+	for (i = 0; i < (strSize); i++)
+		*(copyStr + i) = *(str + i);
+	return (copyStr);
 }
