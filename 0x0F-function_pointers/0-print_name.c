ef8678c5761468726c88c55a62a54b4ee49df924
eagleae7 <spotarinz@gmail.com>
2022-11-16 19:56:57 +0100

    Function pointers fix 10

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,11 @@
  * print_name - prints a name as is
  * @name: name of the person
  * @f: if is null.
+ * Return: nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
+	if ((name == NULL) || (f == NULL))
+		return;
 	f(name);
 }
